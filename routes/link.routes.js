6161c62067383122eefb538db1b9966e91922063
deividsberkolds <deivids.berkolds@gmail.com>
2020-12-08 22:02:55 +0200

    login links, client, server auth

---
+++ routes/link.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const { Router } = require("react-router-dom");
+const Link = require("../models/Link");
+const router = Router();
+
+router.post("/generate", async (req, res) => {});
+
+router.get("/"), async (req, res) => {};
+
+router.get("/:id"), async (req, res) => {};
+
+module.exports = router;
